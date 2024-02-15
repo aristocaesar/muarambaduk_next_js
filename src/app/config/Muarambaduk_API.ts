35cb6f70fa4d6c64e554af0162826cca55c5fc19
aristocaesar <hi@aristoc.space>
2024-02-16 01:14:05 +0700

    add basic auth

--- src/app/config/Muarambaduk_API.ts
+++ src/app/config/Muarambaduk_API.ts
@@ -2,7 +2,13 @@ class MUARAMBADUK_API {
   private static URL = process.env.API_HOST;
 
   public static async Get(url: string) {
-    return await fetch(`${this.URL}/${url}`, { cache: 'no-store' })
+    return await fetch(`${this.URL}/${url}`, {
+      method: 'GET',
+      cache: 'no-store',
+      headers: new Headers({
+        Authorization: `Basic ${process.env.API_AUTH}`,
+      }),
+    })
       .then((result) => result.json())
       .catch((err) => {
         throw new Error(err);
